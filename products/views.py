c76e0b5f046237c3b1956f7649a40a3f7d12c568
Sajjad <47275000+sajjadkargar6131@users.noreply.github.com>
2025-03-06 15:09:35 +0330

    fix template theme

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.shortcuts import render
 from django.views import generic
 from .models import Product
@@ -13,3 +14,9 @@ class ProductDetailView(generic.DetailView):
     template_name = 'products/product_detail.html'
     queryset = Product.objects.filter(active=True)
     context_object_name = 'product'
+    
+    def get_context_data(self, **kwargs) :
+        context = super().get_context_data(**kwargs)
+        context['comments'] = self.object.comments.filter(active=True)
+        context['comments_count'] = context['comments'].count()
+        return context
