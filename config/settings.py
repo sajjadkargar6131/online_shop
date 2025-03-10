02115df6770bce1e415f7ce37d59040d78b1ae89
Sajjad <47275000+sajjadkargar6131@users.noreply.github.com>
2025-03-10 22:33:38 +0330

    Add Persian language translation support

--- config/settings.py
+++ config/settings.py
@@ -145,7 +145,7 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa'
 
 TIME_ZONE = 'UTC'
 
@@ -153,7 +153,9 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+LOCALE_PATHS = [
+    BASE_DIR / 'locale',  
+]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
