e731396d491f843d2b91ce01bc73156db0068605
Sajjad <47275000+sajjadkargar6131@users.noreply.github.com>
2025-03-23 12:54:28 +0330

    context_processors and min cart for all pages

--- config/settings.py
+++ config/settings.py
@@ -88,6 +88,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                
+                #Custom
+                'cart.context_processors.cart',
             ],
         },
     },
