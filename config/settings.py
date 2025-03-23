25f3f8b4aaa057c78869487bd04e8a8091aa5d13
Sajjad <47275000+sajjadkargar6131@users.noreply.github.com>
2025-03-24 00:16:15 +0330

    add jalali date

--- config/settings.py
+++ config/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'jalali_date',
     
     'crispy_forms',
     'crispy_bootstrap5',
